8e764e63ee55bd4397ad17b598b2b9320b66e9b5
Drxptz <81253039+Drxptz@users.noreply.github.com>
2021-03-24 16:17:38 -0400

    vfxv

--- index.js
+++ index.js
@@ -30,4 +30,16 @@ client.on("message", msg => {
 
 });
 
+bot.user.setStatus('available')
+bot.user.setPresence({
+    game: {
+        name: 'with depression',
+        type: "STREAMING",
+        url: "https://www.youtube.com/watch?v=2gktWH4xi1Q&ab_channel=TeeZaar"
+    }
+}
+
+
+
+
 client.login(process.env.BOT_TOKEN);
